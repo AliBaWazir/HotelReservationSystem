2ec72aff89f7f26c8b30c8ff62ba093c46253fac
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-02 00:29:47 -0400

    Added copyright notices

--- customer.py
+++ customer.py
@@ -1,8 +1,20 @@
-#######################################################################
-#   customer.py
-#   This is the Customer class file
-#   Written by: Ali Ba Wazir, May 2018
-#######################################################################
+'''
+    This is the Customer class file
+    Copyright (C) <2018>  <Ali Ba Wazir>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 class Customer():
     customers = [] # temporary list to act as database
